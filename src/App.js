f29fe83d3536668bee3eb0b91cbfc3b0aef0465d
robertogomesbr <robertogomes0277@gmail.com>
2026-06-11 14:03:15 -0300

    fix: corrigir App.js para funcionar permissões de admin e professor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-
+import { setupAxiosInterceptors } from './views/util/AuthenticationService.js';
 import { ToastContainer } from 'react-toastify';
 import Rotas from './Rotas';
 
+setupAxiosInterceptors();
 
 function App() {
   return (
     <div className="App">
-    <Rotas/>
-    <ToastContainer /> 
+      <Rotas />
+      <ToastContainer />
     </div>
   );
 }
